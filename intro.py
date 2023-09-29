77f5485c7439046ccd4c509f35023ca79127a0e0
mohitmittal10 <143540225+mohitmittal10@users.noreply.github.com>
2023-09-29 11:42:06 +0530

    day 2 of industrial training @upflairs

---
+++ intro.py
@@ -0,0 +1,7 @@
+print(2+3)
+print("hello world")
+x=100
+a=3.46
+b="hii"
+print(x,a,b)
+print(type(b))
---
+++ task1.py
@@ -0,0 +1,4 @@
+n1=input("enter first num: ")
+n2=input("enter second num: ")
+sum=int(n1)+int(n2)
+print(sum)
